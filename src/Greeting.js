0a980e9029feb1727665f579e9df4cefbd4198c0
leolpaz <leonardolpaz95@gmail.com>
2022-02-16 02:05:20 -0300

    Create front end

---
+++ src/Greeting.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchData } from "./redux/greetings/greetings"
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greeting = useSelector((state => state.greetingsReducer))
+  useEffect(() => {
+    dispatch(fetchData())
+  }, [])
+  return(
+    <h1>
+      {greeting[0]}
+    </h1>
+  )
+}
+
+export default Greeting
